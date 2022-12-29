6ed1c67ce02c59e49b2a5231f1898df50c9f6936
kangwook <rlarkddnr1686@naver.com>
2022-12-29 22:18:23 +0900

    Entity 생성 및 mapping 작업

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name="delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // Enum쓸때 어노테이션 붙이기 - EnumType에는 String 과 ordinal이 있는데 절대 ordinal쓰면안됨(순서에 밀려날 일 없음)
+    private DeliveryStatus status; //READY, COMP
+}

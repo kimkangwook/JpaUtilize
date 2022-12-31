821c2570e2f48a3b6879d65f109c0f564fd24389
kangwook <rlarkddnr1686@naver.com>
2022-12-31 16:35:34 +0900

    Item 도메인 비즈니스 로직 추가 - Item 도메인의 변수 stockQuantity(재고)에 대한 비즈니스 로직추가      - 재고 더하기, 빼기      - 재고가 Item 도메인의 변수이므로 그에 대한 비즈니스 로직도 Item 도메인에서 추가해줘야함
    ItemRepository 생성
    - Item 저장
          - Item id가 존재하지 않으면 persist로 영속성 컨텍스트에 추가
          - Item id가 존재하면 db에 저장된 Item을 수정하는 걸로 생각
    - Item 단건조회
    - Item List로 가져오기

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId()==null) {
+            em.persist(item); // id값이 없으면 jpa에 등록할 때 generatevalue를 이용해 넣어줌
+        } else {
+            em.merge(item); // 이미 db에 저장된 item을 수정하는 느낌
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}

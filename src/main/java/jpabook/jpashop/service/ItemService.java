90e6c3f482e1add85ca21c79511273cc1e689fcd
kangwook <rlarkddnr1686@naver.com>
2023-01-02 14:19:17 +0900

    ItemService 생성 - 단순하게 ItemRepository에게 위임하는 역할임 - 이때는 Controller에서 바로 Repository로 접근하는 것을 고려해볼만함

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service;
+
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+// 단순하게 repository에 위임하는 역할임. 이 부분은 Controller에서 바로 Repository에 접근하는 것을 고려해볼만함
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
